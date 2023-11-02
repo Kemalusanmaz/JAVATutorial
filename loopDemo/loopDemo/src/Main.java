ac9cebd0161aac0dede10d058b949a34936fc9ee
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-02 15:10:36 +0300

    _While Döngüsüyle Çalışmak

--- loopDemo/loopDemo/src/Main.java
+++ loopDemo/loopDemo/src/Main.java
@@ -5,15 +5,23 @@ public class Main {
         for (int i=1;i<10;i++){ //sayaç, döngünün bitiş koşulu, döngünün iterasyon koşulu,
             System.out.println(i);
         }
-        System.out.println("Döngü bitti.");
+        System.out.println("For Döngüsü bitti.");
 
-        for (int i=1;i<10;i+=2){ //sayaç, döngünün bitiş koşulu, döngünün iterasyon koşulu,
+//        for (int i=1;i<10;i+=2){ //sayaç, döngünün bitiş koşulu, döngünün iterasyon koşulu,
+//            System.out.println(i);
+//        }
+//        System.out.println("Tek sayılar döngüsü bitti.");
+//        for (int i=0;i<10;i+=2){ //sayaç, döngünün bitiş koşulu, döngünün iterasyon koşulu,
+//            System.out.println(i);
+//        }
+//        System.out.println("Çift sayılar döngüsü bitti.");
+
+        //while döngüsü
+        int i =1; //sayaç = başlangıç noktası
+        while (i <10){ //bitiş koşulu
             System.out.println(i);
+            i++; //döngünün iterasyon koşulu
         }
-        System.out.println("Tek sayılar döngüsü bitti.");
-        for (int i=0;i<10;i+=2){ //sayaç, döngünün bitiş koşulu, döngünün iterasyon koşulu,
-            System.out.println(i);
-        }
-        System.out.println("Çift sayılar döngüsü bitti.");
+        System.out.println("While döngüsü bitti.");
     }
 }

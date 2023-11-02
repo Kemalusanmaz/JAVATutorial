dae95df80bcc87584b57f36017a1757ada17d075
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-02 10:40:17 +0300

    Helloworld projesi oluşturulmuştur. Terminale string yazdırma işlemi yapılmıştır.

---
+++ javaHelloWorld/HelloWorld/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello World");
+        System.out.printf("Hello World 2");
+        //Intellisense yapısında sınıflandırma vardır. System.out sınıfı içinde bulunan printf fonksiyonu kullanılır.
+
+    }
+}

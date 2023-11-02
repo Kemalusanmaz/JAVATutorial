90844f573f3f3f88bb70dd66b87d19302c0afbf3
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-02 10:56:16 +0300

    Degisikenler ve Degisken Kullanım İhtiyacı

---
+++ variables/variables/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    // reusaability => tekrarlayarak kullanılabilirlik.
+    public static void main(String[] args) {
+        //case sensitive bir dildir yani büyük küçük harf duyarlıdır.
+
+        System.out.println("Öğrenci sayım : 10");
+        System.out.println("Öğrenci sayım : 10");
+        System.out.println("Öğrenci sayım : 10");
+        System.out.println("Öğrenci sayım : 10");
+
+        int ogrenciSayisi = 11; //integer (tam sayı) türünde değişken tanımlanır.
+        String mesaj = "Öğrenci Sayısı : ";
+
+        System.out.println(mesaj + ogrenciSayisi); //string + integer
+        System.out.println(mesaj + ogrenciSayisi);
+    }
+}

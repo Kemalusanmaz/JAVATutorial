02918a65e541ce079e1c6e8433288f4527db29b7
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-02 11:25:54 +0300

    Temel Veri Tipleriyle Çalışmak

---
+++ dataTypes/dataTypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 12; //4 byte yer kaplar.
+        byte sayi2 = 10; // 1 byte yer kaplar.
+        double sayi3 = 12.5; // 8 byte yer kaplar.
+
+        char karakter = 'A'; //tek karakter tutulur. 1 byte.
+        String sehir = "Ankara"; //karakter topluluğu tutulur.
+
+        boolean dogru = true ; //doğru-yanlış 1 byte yer kaplar.
+        boolean yanlis = false ;
+        
+        //genelde int ve double ile çalışılır.
+    }
+}

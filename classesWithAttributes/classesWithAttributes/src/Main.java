b7f48104e7905af2c510a1540c77e103179509ca
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-06 17:24:15 +0300

    _Constructor ile Çalışmak

--- classesWithAttributes/classesWithAttributes/src/Main.java
+++ classesWithAttributes/classesWithAttributes/src/Main.java
@@ -1,26 +1,38 @@
 public class Main {
 
     public static void main(String[] args) {
-    Product product = new Product();
-    //product.name = "Laptop"; //class içindeki değişlenler private olrak yapıldığı için başka bir class bunu kullanamıyor.
+
+    //---- Constraction Blok - Yapıcı Bloklar ----
+    //yapıcı blok içinde tanımlanmış operasyonu çalıştırır. Defaultta bir şey yoktur.
+
+    //yapıcı blok içinde attributler tanımlanır.
+    Product product = new Product(1,"Laptop","ASUS Laptop",5000,3,"siyah");
+    System.out.println(product.getKod());
+
+    //---- Encapsulation---
+
+    /*Product product = new Product();
     product.setName("Laptop");
-    //product.id = 1;
     product.setId(1); //encapsulation yapılıyor. id set ediliyor.
-    //product.description = "ASUS Laptop";
     product.setDescription("ASUS Laptop");
-    //product.price = 5000;
     product.setPrice(5000);
-    //product.stockAmount = 3;
-    product.setStockAmount(3);
+    product.setStockAmount(3);*/
 
+    //----- public değişkenler ile classı kullanmak ----
 
+    /*Product product = new Product();
+    product.name = "Laptop"; //class içindeki değişlenler private olrak yapıldığı için başka bir class bunu kullanamıyor.
+    product.description = "ASUS Laptop";
+    product.id = 1;
+    product.price = 5000;
+    product.stockAmount = 3;
+     */
     ProductManager productManager = new ProductManager();
 
     productManager.add(product);
 
     //productManager.add2(1,"","",2,200); yeni özellik classta eklenip fonksyon argümanı olarak tanımlanmadığı için hata verir. Dinamik değildir.
 
-    System.out.println(product.getKod());
 
     }
 }
--- classesWithAttributes/classesWithAttributes/src/Product.java
+++ classesWithAttributes/classesWithAttributes/src/Product.java
@@ -1,6 +1,22 @@
 public class Product {
     //class public olduğu için bu classa her yerden ulaşılabilir.
 
+    //Her classın kendi ismiyle bir constractor'u vardır default tanımlanmıştır.
+    public Product(int id, String name, String description, double price, int stockAmount, String renk ){ //constructor => yapıcı metot
+        //enkapsüle etmek yerine attributeler yapıcı fonksiyon argümanı olarak tanımlanabilir.Main de new değişkenine de bu argümanlar verilmelidir.
+
+        //construckter kullanımında getter gibi class içinde bulunan id değişkenini yapıcı blok içinde tanımlanan id değişkenine eşitler.
+        this.id = id;
+        this.renk = renk;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.stockAmount = stockAmount;
+
+        System.out.println("Yapıcı blok çalıştı");
+
+    }
+
     // bir class aynı zamanda attribute barındırma özelliği vardır.
     // attribute | field
     private int id; // sadece tanımlandığı blokta geççerlidir. // get ve set edilirken karışmamsı için değişkenler _eklenir.

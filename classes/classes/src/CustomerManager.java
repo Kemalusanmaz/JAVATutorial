f1913972bdc40685c91004fd503e4edfc3b7e936
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-06 13:59:23 +0300

    _Class Nedir?

---
+++ classes/classes/src/CustomerManager.java
@@ -0,0 +1,12 @@
+public class CustomerManager { //src'dan new class işlemi yapılır
+
+    public void Add(){ //ekleme kodları
+        System.out.println("Müşteri eklendi.");
+    }
+    public void Remove(){ //ekleme kodları
+        System.out.println("Müşteri silindi.");
+    }
+    public void Update(){ //ekleme kodları
+        System.out.println("Müşteri güncellendi.");
+    }
+}

b0a0e93430486dbc5b9e23ec466983e359a25840
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-08 15:52:10 +0300

    _Kendi Paketimizi Oluşturmak

--- packagesDemo/packagesDemo/src/Main.java
+++ packagesDemo/packagesDemo/src/Main.java
@@ -1,4 +1,10 @@
 //JAVA'nın içinde tanımlı paketler vardır => built-in
+//projede oluşturulan katmanlar sıklıkla paket haline getirilir.
+
+import matematik.DortIslem;
+import matematik.Logaritma;
+
+import matematik.* ;//paketin içindeki classları tek tek import etmek yerine .* şeklinde içindeki tüm classlar çağrılır
 
 import java.util.Scanner; // built-in paket import edilir. scanner konsol üzerinden kullanıcı data alınmasını sağlar.
 
@@ -11,5 +17,13 @@ public class Main {
         String isim = scanner.nextLine(); //Konsoldan kullanıcı girişi yapılmalıdır.
 
         System.out.println("Merhaba " + isim);
+
+        //matematik paketinin içinde oluşturulan Dortıslem classının referansı alınır.
+        // Classın içindeki operasyonu gerçekleştirmek için topla fonksiyonu çağrılır.
+        DortIslem dortIslem = new DortIslem();
+        dortIslem.topla(5,10);
+        Logaritma logaritma= new Logaritma();
+        logaritma.logaritmaHesapla();
+
     }
 }

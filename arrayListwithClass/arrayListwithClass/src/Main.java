b0af62ee29dc6abd7c55ddb16f8d61bc21b4550e
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-10 11:11:11 +0300

    _Demo - Classlar ile ArrayList

---
+++ arrayListwithClass/arrayListwithClass/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //Customer nesnesi içinde bir yapıcı blok oluşturulur. Bu classın alacağı parametreler ayarlanır.
+        ArrayList<Customer> customers = new ArrayList<Customer>(); //Class'ın arraylist içinde bir referansı oluşturulur.
+
+        Customer engin = new Customer(1,"Engin","Demiroğ");
+        customers.add(engin);
+
+        customers.add(new Customer(2,"Derin","Demiroğ")); 
+        customers.add(new Customer(3,"Salih","Demiroğ"));
+
+
+        customers.remove(engin);
+
+        for (Customer customer:customers){
+            System.out.println(customer.id + " " + customer.firstName + " " + customer.lastName);
+
+        }
+    }
+}

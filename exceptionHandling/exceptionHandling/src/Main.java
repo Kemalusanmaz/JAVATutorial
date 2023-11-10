e0ab0ea347807df38a0bdc2333cdfcb9526eeaf1
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-10 14:35:45 +0300

    _Demo - Exception Hiyerarşisi

--- exceptionHandling/exceptionHandling/src/Main.java
+++ exceptionHandling/exceptionHandling/src/Main.java
@@ -6,14 +6,20 @@ public class Main {
         try{ //try bloğunun içindeki kotu çalıştırmayı dene
             int[] sayilar = new int[]{1,2,3};
             System.out.println(sayilar[5]);
-        }catch (Exception exception){ // kodlarda hata gelirse bu bloğa geçilir.
+        }//catch (Exception exception){ // kodlarda hata gelirse bu bloğa geçilir.
+        catch (ArrayIndexOutOfBoundsException exception){ //çıkan hata tipi de exception tipi olarak yazılabilir.
             // try bloğundaki hata catch bloğuna parametre olarak verilir.
 
-            System.out.println("Hata oluştu");
+            System.out.println("Array Hata oluştu");
 
             System.out.println(exception.getMessage());
 
-        }finally { //son çalışacak blok. ister try ister catch bloğu çalışsın finally bloğu en sonda her türlü çalışır.
+        }catch (StringIndexOutOfBoundsException exception){ //birden çok catch bloğu kullanılabilir.
+            System.out.println("String Hata oluştu");
+        }catch (Exception exception){//ilk exception tipleri çalışmazsa yani hata catch bloklarında tanımlanmamışsa Exception tipiyle loglanır.
+            System.out.println("Loglandı" + exception);
+        }
+        finally { //son çalışacak blok. ister try ister catch bloğu çalışsın finally bloğu en sonda her türlü çalışır.
             //veritabanına bağlanırken hata oluştu. veri tabanına bağlantı oluştuurlduktan sonra bağlantı kapatılması gerekir. Burada kullanılabilir.
             System.out.println("Her türlü çalışır");
         }

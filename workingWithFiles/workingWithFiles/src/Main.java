d9a7472ece45aa6aca5cbadd344cce15e60b45c5
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-13 08:11:08 +0300

    _Dosyaların Okunması

--- workingWithFiles/workingWithFiles/src/Main.java
+++ workingWithFiles/workingWithFiles/src/Main.java
@@ -1,10 +1,13 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner; //dosyanın okunması için gereken paket
 
 public class Main {
     public static void main(String[] args) {
         //createFile();
-        getFileInfo();
+        //getFileInfo();
+        readFile();
 
     }
     //dosya oluşturmak
@@ -37,6 +40,20 @@ public class Main {
             System.out.println("Dosya boyutu (byte): " + file.length()); //dosyanın boyunu veriri
         }
 
+    }
+
+    public static void readFile(){
+        File file = new File("D:\\KEMAL_PROJECTS\\JAVA_ile_Programlamaya_Giris\\javaDemos\\Files\\students.txt");
+        try {
+            Scanner reader = new Scanner(file); // Scanner classının referansı alınır. ve io operasyonu try catch içine alınmak zorundadır.
+            while (reader.hasNextLine()){ //her satır okunur bir sonraki satır okunabilirse true döner.
+                String line = reader.nextLine(); //her satır sırayla okunur.
+                System.out.println(line);
+            }
+            reader.close(); //doyas işi biitince kapatılması gerekir bellekten uçurulması gerekir.
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     }

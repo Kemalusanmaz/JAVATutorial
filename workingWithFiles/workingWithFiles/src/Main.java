8f3f8cbf0170c17e896cad7bebdff5d1d06cdadd
Kemal Usanmaz <kemalusanmaz1996@gmail.com>
2023-11-13 07:53:29 +0300

    _Dosya Bilgilerini Okumak

--- workingWithFiles/workingWithFiles/src/Main.java
+++ workingWithFiles/workingWithFiles/src/Main.java
@@ -3,6 +3,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        //createFile();
+        getFileInfo();
+
+    }
+    //dosya oluşturmak
+    public static void createFile(){
         //dosya yolu verilir.
         File file = new File("D:\\KEMAL_PROJECTS\\JAVA_ile_Programlamaya_Giris\\javaDemos\\Files\\students.txt");
         try {
@@ -18,5 +24,22 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
-}
+    public static void getFileInfo(){
+        File file = new File("D:\\KEMAL_PROJECTS\\JAVA_ile_Programlamaya_Giris\\javaDemos\\Files\\students.txt");
+
+        if (file.exists()){ //dosya varsa true yoksa false döner
+            System.out.println("Dosya adı: " + file.getName()); //getName dosya adını return eder.
+            System.out.println("Dosya yolu: " + file.getAbsolutePath()); //getAbsolutePath dosya yolunu return eder.
+            System.out.println("Dosya yazılabilir mi: " + file.canWrite()); //dosya sadece read-only ise false write and read ise true döner
+            System.out.println("Dosya okunabilir mi: " + file.canRead()); //dosya sadece okunabilirse true döner
+            System.out.println("Dosya boyutu (byte): " + file.length()); //dosyanın boyunu veriri
+        }
+
+    }
+
+    }
+
+
+
